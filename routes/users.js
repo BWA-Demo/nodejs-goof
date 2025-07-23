3f8fb68379a56f341ac8c138bc985de60821100b
nirw-snyk <130424397+nirw-snyk@users.noreply.github.com>
2025-07-23 11:26:20 +1000

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -43,3 +43,9 @@ router.post('/', async (req, res, next) => {
     next();
   }
 })
+// Route vulnerable to XSS
+router.get('/xss', (req, res) => {
+  const name = req.query.name || 'world';
+  // Vulnerable: unsanitized user input is rendered directly in HTML
+  res.send(`<h1>Hello ${name}</h1>`);
+});
