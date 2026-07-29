85e00e0e10680750405ccb0884dbdf5e94eb77d2
nirw-snyk <130424397+nirw-snyk@users.noreply.github.com>
2026-07-29 21:18:50 +1000

    Add greeting route with name query parameter
    Add a new route to greet users with a name parameter.

--- routes/users.js
+++ routes/users.js
@@ -43,3 +43,10 @@ router.post('/', async (req, res, next) => {
     next();
   }
 })
+
+router.get('/Hello', (req, res) => {
+  const name = req.query.name || 'World';
+  // Intentionally vulneraareble: do not sanitize input
+  res.send(`<h1>Hello ${name}</h1>`);
+})
+
